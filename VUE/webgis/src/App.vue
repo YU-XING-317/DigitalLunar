b3c2920127282ef809c05cb46fed813f639196b9
yuxing <skyeye317@163.com>
2024-04-01 14:05:34 +0800

    vue初始化

--- VUE/webgis/src/App.vue
+++ VUE/webgis/src/App.vue
@@ -1,93 +1,30 @@
 <template>
-  <div id="app">
-    <el-container class="app-out-pannel">
-      <el-header class="sys-header">WebGIS-数字月球系统</el-header>
-      <el-container class="app-content-pannel">
-        <el-aside class="sys-menu">
-          <el-menu
-            default-active="1"
-            class="el-menu-vertical-demo"
-            @select="handleMenuSelect"
-            :collapse="true"
-            background-color="#606266"
-          >
-            <el-menu-item index="1">
-              <i class="el-icon-monitor"></i>
-              <span slot="title">地图大屏</span>
-            </el-menu-item>
-            <el-menu-item index="2">
-              <i class="el-icon-picture-outline"></i>
-              <span slot="title">一张图</span>
-            </el-menu-item>
-            <el-menu-item index="3">
-              <i class="el-icon-map-location"></i>
-              <span slot="title">三维展示</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-              <i class="el-icon-setting"></i>
-              <span slot="title">数据管理</span>
-            </el-menu-item>
-          </el-menu>
-        </el-aside>
-
-        <el-main class="sys-content">
-          <router-view></router-view>
-          <!-- <MapView /> -->
-        </el-main>
-      </el-container>
-    </el-container>
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-// import MapView from './components/common/MapView.vue';
-export default {
-  name: 'App',
-  components: {
-    // MapView,
-  },
-  methods: {
-    handleMenuSelect(index) {
-      console.log(index);
-      if (index === '1') {
-        //跳转到首页大屏
-        this.$router.push('/');
-      } else if (index === '2') {
-        //跳转到一张图
-        // this.$router.push() 可以通过修改url实现路由跳转
-        this.$router.push('/onemap');
-      }
-    },
-  },
-};
-</script>
-
 <style>
-html,
-body,
 #app {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  margin: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-.app-out-pannel,
-.app-content-pannel {
-  height: 100%;
+
+nav {
+  padding: 30px;
 }
-.sys-header {
-  background-color: #303133;
-  line-height: 60px;
-  height: 60px;
-  color: #fff;
-  font-size: 600;
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
-.sys-menu {
-  background-color: #606266;
-  width: 60px !important;
-  overflow: hidden;
-}
-.sys-content {
-  padding: 5px;
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- VUE/webgis/src/store/index.js
+++ VUE/webgis/src/store/index.js
@@ -1,27 +1,14 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import { createStore } from 'vuex'
 
-Vue.use(Vuex)
-
-const state = {
-    _defaultaMapView: '',   //默认地图view
-}
-
-const getters = {
-    _getDefaultMapView() {
-        return state._defaultMapView
-    }
-}
-const mutations = {
-    _setDefaultMapView(state, value) {
-        state._defaultMapView = value
-    }
-}
-
-const store = new Vuex.Store({
-    state,
-    getters,
-    mutations
-});
-
-export default store;
+export default createStore({
+  state: {
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
